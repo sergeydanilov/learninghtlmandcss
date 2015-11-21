51f8604e47f037c6db59f57b6181c1a56d8f81c8
Sergey Danilov <sergeydanilov@gmail.com>
2015-11-21 13:43:15 +0800

    added for js overview some first page

---
+++ tp_rest/src/main/resources/public/js/myscripts.js
@@ -0,0 +1,9 @@
+//var thisVariable = 3;
+//counter = counter + 1;
+
+alert("Hello world!");
+
+document.write("An incredibly really \
+very long greeting to the world");
+
+console.log("sssssssssssssssssssssssssssssssssss");
