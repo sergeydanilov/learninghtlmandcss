0e5dd39d7229e3c1f30c74db41ce8ecde446642a
Sergey Danilov <sergeydanilov@gmail.com>
2015-11-26 12:38:47 +0800

    fixed send request script

--- tp_rest/src/main/resources/public/js/rmyscripts.js
+++ tp_rest/src/main/resources/public/js/rmyscripts.js
@@ -3,18 +3,25 @@
  */
 
 function tryMyLuck() {
+
+
     var request = new XMLHttpRequest();
-    request.open("GET", "/firstData", false);
-    request.send();
+    try {
+
+        request.open("GET", "/firstData", false);
+        request.send();
 
 
 // wait for the request status to be returned
 
-    if (request.status != 200) {
-        alert("Error " + request.status + " - " + request.statusText);
-    } else {
-        alert(request.responseText);
-        console.log(equest.responseText);
+        if (request.status != 200) {
+            alert("Error " + request.status + " - " + request.statusText);
+        } else {
+            alert(request.responseText);
+            console.log(request.responseText);
+        }
+    } catch(exception) {
+        console.lof("exception occurs = " + exception);
     }
 }
 
